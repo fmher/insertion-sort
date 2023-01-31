6adc1afd54a399706fe496c337dd7c295e127652
weston-bailey <wdbailey87@gmail.com>
2023-01-31 11:59:28 -0800

    pep8ifies

--- sort.py
+++ sort.py
@@ -1,16 +1,20 @@
 # use to check if the array is sorted
 def is_sorted(ls):
-  for i in range(len(ls) - 1):
-    if ls[i] > ls[i + 1]:
-      return False
-  return True
+    for i in range(len(ls) - 1):
+        if ls[i] > ls[i + 1]:
+            return False
+    return True
+
 
 def insertion_sort(li):
     # implement your algorithm here
-     pass
+    pass
+
 
 # for testing
-li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
+li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4,
+      41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
 
 insertion_sort(li)
-print(is_sorted(li)) # should return True, because insertion sort is an in-place sort
+# should return True, because insertion sort is an in-place sort
+print(is_sorted(li))
